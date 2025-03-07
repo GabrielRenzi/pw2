f63bd1dbdb44dd147b1101eacd014054b1ae1f36
Gabriel <gabriel.renzi@unidavi.edu.br>
2025-03-06 21:39:29 -0300

    laravel

--- laravel/olamundo/routes/web.php
+++ laravel/olamundo/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContatoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::resource('/contato', ContatoController::class);
